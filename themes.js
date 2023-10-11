96a8c9c7a2650de610133ed5782f5cc9b7c0b3fe
Avinash Badaramoni <80014354+badaramoni@users.noreply.github.com>
2023-10-11 15:52:35 -0400

    Add files via upload
    theam

---
+++ themes.js
@@ -0,0 +1,18 @@
+export const themes = {
+    Android: {
+      backgroundColor: '#2b2b2b',
+      color: '#a9b7c6',
+      fontFamily: '"JetBrains Mono", monospace'
+    },
+    iOS: {
+      backgroundColor: '#282C34',
+      color: '#ABB2BF',
+      fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto'
+    },
+    Web: {
+      backgroundColor: '#1E1E1E',
+      color: '#D4D4D4',
+      fontFamily: '"Courier New", monospace'
+    }
+  };
+  
